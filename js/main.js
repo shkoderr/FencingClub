943ff8ca9bc17e5cb98fac6696d66d87266c519a
Timur Birgalin <elecktrohachyck@gmail.com>
2023-03-22 19:01:38 +0400

    modified accordeon to make it smooth

--- js/main.js
+++ js/main.js
@@ -7,21 +7,23 @@ for (let i = 0; i < headers.length; i++) {
   headers[i].addEventListener('touchstart', showContent)
 }
 
-//Показываем ответ: 
+//Показываем ответ, изменяя высоту контента: 
 function showContent() {
   this.classList.toggle('active');
   const accordeonContent = this.nextElementSibling;
 
-  if (accordeonContent.style.display === 'block') {
-    accordeonContent.style.display = 'none';
+  if (accordeonContent.style.maxHeight) {
+    accordeonContent.style.maxHeight = null;
   } else {
+    //Сначала пробежимся циклом 'for' и закроем все остальные ответы: 
     const accordeonContents = document.querySelectorAll('.accordeon__content');
     for (let i = 0; i < accordeonContents.length; i++) {
-      if (accordeonContents[i] !== accordeonContent && accordeonContents[i].style.display === 'block') {
-        accordeonContents[i].style.display = 'none';
+      if (accordeonContents[i] !== accordeonContent && accordeonContents[i].style.maxHeight) {
+        accordeonContents[i].style.maxHeight = null;
         accordeonContents[i].previousElementSibling.classList.remove('active');
       }
     }
-    accordeonContent.style.display = 'block';
+    //Теперь откроем необходимый ответ: 
+    accordeonContent.style.maxHeight = accordeonContent.scrollHeight + 'px';
   }
 }
